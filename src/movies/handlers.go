dcbe087f469c7d1338ebf9bce545353787a485e6
Abigail Fernandes <abigailferns94@gmail.com>
2017-07-04 22:54:20 +0530

    Update handlers.go

--- src/movies/handlers.go
+++ src/movies/handlers.go
@@ -13,14 +13,14 @@ import (
 // }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	// todos := Todos{
-	// 	Todo{Name: "Write presentation"},
-	// 	Todo{Name: "Host meetup"},
-	// }
-	movies := getMovies()
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+// 	movies := getMovies()
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(movies); err != nil {
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
 }
