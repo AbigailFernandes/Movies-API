ddc1019ef023e31cb5ab6381a855c2615c7675df
Abigail Fernandes <abigailferns94@gmail.com>
2017-07-04 22:51:10 +0530

    Update handlers.go

--- src/movies/handlers.go
+++ src/movies/handlers.go
@@ -17,27 +17,6 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 	// 	Todo{Name: "Write presentation"},
 	// 	Todo{Name: "Host meetup"},
 	// }
-
-	// var movies []Movie
-	// csvFile, _ := os.Open("../movie_metadata.csv")
-	// reader := csv.NewReader(bufio.NewReader(csvFile))
-	// for {
-	// 	// fmt.Println("hellosrse")
-	// 	line, error := reader.Read()
-	// 	if error == io.EOF {
-	// 		break
-	// 	} else if error != nil {
-	// 		fmt.Println("hellofff")
-	// 		log.Fatal(error)
-	// 	}
-	// 	movies = append(movies, Movie{
-	// 		MovieTitle:   line[11],
-	// 		DirectorName: line[1],
-	// 		IMDBScore:    line[25],
-	// 	})
-	// }
-	// movieJson, _ := json.Marshal(movies)
-	// fmt.Println(string(movieJson))
 	movies := getMovies()
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
