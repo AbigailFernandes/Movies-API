98ba928114a66ac454dfdf0ab9b5581c6fd113c2
Abigail Fernandes <abigailferns94@gmail.com>
2017-07-05 23:48:38 +0530

    Added new dataset of adults

--- src/movies/handlers.go
+++ src/movies/handlers.go
@@ -8,16 +8,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// func Index(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "Welcome!")
-// }
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
 	todos := Todos{
 		Todo{Name: "Write presentation"},
 		Todo{Name: "Host meetup"},
 	}
-// 	movies := getMovies()
+	// 	movies := getMovies()
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
--- src/movies/todo.go
+++ src/movies/todo.go
@@ -1,17 +1,19 @@
 package main
 
-// type Todo struct {
-// 	Name      string    `json:"name"`
-// 	Completed bool      `json:"completed"`
-// 	Due       time.Time `json:"due"`
-// }
+import "time"
 
-type Movie struct {
-	MovieTitle   string `json:"movietitle"`
-	DirectorName string `json:"directorname"`
-	IMDBScore    string `json:"imdbscore"`
+type Todo struct {
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	Due       time.Time `json:"due"`
 }
 
-type Movies []Movie
+// type Movie struct {
+// 	MovieTitle   string `json:"movietitle"`
+// 	DirectorName string `json:"directorname"`
+// 	IMDBScore    string `json:"imdbscore"`
+// }
+//
+// type Movies []Movie
 
-// type Todos []Todo
+type Todos []Todo
